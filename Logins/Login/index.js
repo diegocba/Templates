a3e2f3e858b6d2d67f16fc08072e69bd71e417d4
diego <diegocab456@gmail.com>
2019-07-08 01:27:49 -0300

    adding login interface

---
+++ Logins/Login/index.js
@@ -0,0 +1,8 @@
+$(".show-login-btn").on("click", function(){
+    $(".login-box").toggleClass("showed");
+});
+$(".hide-login-btn").on("click", function(){
+    $(".login-box").toggleClass("showed");
+});
+
+// toggleClass("classname") -> agrega una subclase css al html
